d875863f69d406e47a72eefa790ac3c68b6ccd21
lukas3540 <lukaslorentz123@gmail.com>
2026-01-31 13:18:57 +0100

    main with loop and delta time

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
+#include <chrono>
+#include <csignal>
 #include <iostream>
+#include <thread>
+
+volatile sig_atomic_t gSignalStatus = 0;
+
+void signalHandler(int sig) { gSignalStatus = sig; }
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    signal(SIGINT, signalHandler);
+    bool running = true;
+    auto last_tick = std::chrono::steady_clock::now();
+    std::cout << "Tracker is running... (Press ctr+c to stop!) \n";
+    while (running) {
+        if (gSignalStatus != 0) {
+            running = false;
+        }
+        auto now = std::chrono::steady_clock::now();
+        std::chrono::duration<double> delta = now - last_tick;
+        std::cout << delta.count() << std::endl;
+
+        last_tick = std::chrono::steady_clock::now();
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    }
+
+    std::cout << "END" << std::endl;
+
     return 0;
 }
